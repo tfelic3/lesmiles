7d47fac9c7a03c5b56d6da4ff5126b356fe30675
tfelic3 <felicien.tesalon@gmail.com>
2017-07-04 20:39:44 -0400

    Add files via upload

---
+++ les/style/main.js
@@ -0,0 +1,84 @@
+console.log('You will not be around tomorrow');
+	
+function PlaySound(soundobj) {
+var thissound=document.getElementById(soundobj);
+thissound.play();
+}
+function StopSound(soundobj) {
+var thissound=document.getElementById(soundobj);
+thissound.pause();
+thissound.currentTime = 0;
+}
+
+
+function PlaySound1(soundobj1) {
+var thissound=document.getElementById(soundobj1);
+thissound.play();
+}
+function StopSound1(soundobj1) {
+var thissound=document.getElementById(soundobj1);
+thissound.pause();
+thissound.currentTime = 0;
+}
+
+function PlaySound2(soundobj2) {
+var thissound=document.getElementById(soundobj2);
+thissound.play();
+}
+function StopSound2(soundobj2) {
+var thissound=document.getElementById(soundobj2);
+thissound.pause();
+thissound.currentTime = 0;
+}
+
+function PlaySound3(soundobj3) {
+var thissound=document.getElementById(soundobj3);
+thissound.play();
+}
+function StopSound3(soundobj3) {
+var thissound=document.getElementById(soundobj3);
+thissound.pause();
+thissound.currentTime = 0;
+}
+
+function PlaySound4(soundobj4) {
+var thissound=document.getElementById(soundobj4);
+thissound.play();
+}
+function StopSound4(soundobj4) {
+var thissound=document.getElementById(soundobj4);
+thissound.pause();
+thissound.currentTime = 0;
+}
+
+function PlaySound5(soundobj5) {
+var thissound=document.getElementById(soundobj5);
+thissound.play();
+}
+function StopSound5(soundobj5) {
+var thissound=document.getElementById(soundobj5);
+thissound.pause();
+thissound.currentTime = 0;
+}
+
+function PlaySound6(soundobj6) {
+var thissound=document.getElementById(soundobj6);
+thissound.play();
+}
+function StopSound6(soundobj6) {
+var thissound=document.getElementById(soundobj6);
+thissound.pause();
+thissound.currentTime = 0;
+}
+
+function PlaySound7(soundobj7) {
+var thissound=document.getElementById(soundobj7);
+thissound.play();
+}
+function StopSound7(soundobj7) {
+var thissound=document.getElementById(soundobj7);
+thissound.pause();
+thissound.currentTime = 0;
+}
+
+
